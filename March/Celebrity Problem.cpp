2e0863d18caf47a340a64865670187fb8aac25ff
M Wasi <reallywasi@gmail.com>
2024-03-14 14:31:54 +0530

    Create Celebrity Problem.cpp

---
+++ March/Celebrity Problem.cpp
@@ -0,0 +1,121 @@
+A celebrity is a person who is known to all but does not know anyone at a party. If you go to a party of N people, find if there is a celebrity in the party or not.
+A square NxN matrix M[][] is used to represent people at the party such that if an element of row i and column j  is set to 1 it means ith person knows jth person. Here M[i][i] will always be 0.
+Return the index of the celebrity, if there is no celebrity return -1.
+Note: Follow 0 based indexing.
+Follow Up: Can you optimize it to O(N)
+ 
+
+Example 1:
+
+Input:
+N = 3
+M[][] = {{0 1 0},
+         {0 0 0}, 
+         {0 1 0}}
+Output: 1
+Explanation: 0th and 2nd person both
+know 1. Therefore, 1 is the celebrity. 
+
+
+________________________________________________________________________________________________________________________
+
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution 
+{
+    private:
+    bool knows(vector<vector<int> >& M, int a, int b, int n) {
+        if(M[a][b] == 1)
+            return true;
+        else
+            return false;
+    }
+    public:
+    //Function to find if there is a celebrity in the party or not.
+    int celebrity(vector<vector<int> >& M, int n) 
+    {
+        stack<int> s;
+        //step1: push all element in stack
+        for(int i=0; i<n; i++) {
+            s.push(i);
+        }   
+        
+        //step2: get 2 elements and copare them
+        
+        while(s.size() > 1) {
+            
+            int a = s.top();
+            s.pop();
+            
+            int b = s.top();
+            s.pop();
+            
+            if(knows(M,a,b,n)){
+                s.push(b);
+            }
+            else
+            {
+                s.push(a);
+            }
+        }
+        int ans = s.top();
+        //step3: single element in stack is potential celeb
+        //so verify it
+        
+        int zeroCount = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(M[ans][i] == 0)
+                zeroCount++;
+        }
+        
+        //all zeroes
+        if(zeroCount != n)
+            return -1;
+        
+        //column check
+        int oneCount = 0;
+        
+        for(int i=0; i<n; i++) {
+            if(M[i][ans] == 1)
+                oneCount++;
+        }
+        
+        if(oneCount != n-1)
+            return -1;
+        
+        return ans;
+        
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<vector<int> > M( n , vector<int> (n, 0));
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                cin>>M[i][j];
+            }
+        }
+        Solution ob;
+        cout<<ob.celebrity(M,n)<<endl;
+
+    }
+}
+  // } Driver Code Ends
