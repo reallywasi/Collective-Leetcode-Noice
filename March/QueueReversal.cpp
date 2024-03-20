2061ca594bdb8615009e4a1e093dc83cb855a313
M Wasi <reallywasi@gmail.com>
2024-03-20 15:21:17 +0530

    Create QueueReversal.cpp

---
+++ March/QueueReversal.cpp
@@ -0,0 +1,16 @@
+class Solution
+{
+    public:
+    queue<int> rev(queue<int> q)
+    {
+vector<int>ans;
+for(int i=0;i<q.size();i++)
+{
+    ans[i]=q.front();
+    q.pop();
+}
+
+queue<int>x;
+
+    }
+};
