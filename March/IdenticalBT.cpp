58f76384aeef9cd52adcf88d82f1808828bd944e
M Wasi <reallywasi@gmail.com>
2024-03-26 09:45:58 +0530

    Create IdenticalBT.cpp

---
+++ March/IdenticalBT.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+     if(p==NULL && q==NULL)
+     {
+        return true;
+
+     }   
+     if(p!=NULL && q==NULL)
+     {
+        return false;
+        
+     } 
+     if(p==NULL && q!=NULL)
+     {
+        return false;
+        
+     } 
+     bool l=isSameTree(p->left,q->left);
+    bool r=isSameTree(p->right,q->right);
+
+bool val=p->val == q->val;
+
+if(val && l && r )
+{
+    return true;
+}
+else{
+    return false;
+}
+    }
+};
