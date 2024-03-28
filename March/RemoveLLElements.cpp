58c11acf37a83dc8e2db3766981a3c1bcd2651db
M Wasi <reallywasi@gmail.com>
2024-03-28 22:55:58 +0530

    Create RemoveLLElements.cpp

---
+++ March/RemoveLLElements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    ListNode* removeElements(ListNode* head, int val) {
+        ListNode* dummy = new ListNode(0);
+        dummy->next = head;
+        ListNode* prev = dummy;
+        ListNode* curr = head;
+
+        while (curr != nullptr) {
+            if (curr->val == val) {
+                prev->next = curr->next;
+                delete curr;
+                curr = prev->next;
+            } else {
+                prev = curr;
+                curr = curr->next;
+            }
+        }
+
+        head = dummy->next;
+        delete dummy;
+        return head;
+    }
+};
