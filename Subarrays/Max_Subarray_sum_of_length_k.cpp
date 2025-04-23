49ed50a1c59dbbcd350f021215a6c08a75b49c74
M Wasi <reallywasi@gmail.com>
2025-04-23 10:51:55 +0530

    Update Max_Subarray_sum_of_length_k.cpp

--- Subarrays/Max_Subarray_sum_of_length_k.cpp
+++ Subarrays/Max_Subarray_sum_of_length_k.cpp
@@ -119,3 +119,33 @@ for(int i=0;i<=n-k;i++)
 return msum;
     }
 };
+
+
+
+//________________________________________________________________________________________________
+
+without using queue
+
+class Solution {
+public:
+    int maxScore(vector<int>& cardPoints, int k) {
+        int n = cardPoints.size();
+        int windowSize = n - k;
+        int totalSum = 0;
+
+        for (int i : cardPoints) totalSum += i;
+
+        int windowSum = 0;
+        for (int i = 0; i < windowSize; i++) {
+            windowSum += cardPoints[i];
+        }
+
+        int minWindowSum = windowSum;
+        for (int i = windowSize; i < n; i++) {
+            windowSum += cardPoints[i] - cardPoints[i - windowSize];
+            minWindowSum = min(minWindowSum, windowSum);
+        }
+
+        return totalSum - minWindowSum;
+    }
+};
